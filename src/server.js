7b1872dfbbd222a7568e51398b4a9bee4f631ecf
isnastish <paukden12@gmail.com>
2025-01-07 22:41:49 +0100

    created simple express aplication

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log(`Hello world`);
+const express = require("express");
+const app = express();
+
+const port = 5001;
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  /* eslint-disable-next-line no-console */
+  console.log(`username: ${username}, password: ${password}`);
+  res.send("Ok");
+});
+
+app.listen(port, () => {
+  /* eslint-disable-next-line no-console */
+  console.log(`Listening on port: ${port}`);
+});
