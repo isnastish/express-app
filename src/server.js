eb1520e346698c32f3a496d09453a5f6d77ccf2f
isnastish <paukden12@gmail.com>
2025-01-07 23:13:12 +0100

    add todo

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const app = express();
 const port = 5001;
 
 app.post("/login", async (req, res) => {
+  // TODO: This has to be moved into a separate function to
+  // align more with hexagonal architecture.
   const { username, password } = req.body;
-
   /* eslint-disable-next-line no-console */
   console.log(`username: ${username}, password: ${password}`);
   res.send("Ok");
